70f3cb1dfeed94baf7ecbb3ff9517ce7bba5c1d2
inesvar <90340779+inesvar@users.noreply.github.com>
2023-10-09 09:27:22 +0200

    blinking a led on gpioa

--- src/main.c
+++ src/main.c
@@ -1,8 +1,23 @@
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
-#include <stdio.h>
+
+// Define `GPIOA_NODE` as the node whose label is `gpioa` in the device tree
+#define GPIOA_NODE DT_NODELABEL(gpioa)
+
+// Get the struct device pointer to `gpioa` at compile time
+static const struct device * const gpioa_dev = DEVICE_DT_GET(GPIOA_NODE);
 
 int main() {
-    printf("Hello, world\n");
-    while(1) {}
-    return 0;
+  if (!device_is_ready(gpioa_dev)) {
+    return -ENODEV;  // Device is not ready
+  }
+  // Configure the led as an output, initially inactive
+  gpio_pin_configure(gpioa_dev, 5, GPIO_OUTPUT_INACTIVE);
+  // Toggle the led every second
+  for (;;) {
+    gpio_pin_toggle(gpioa_dev, 5);
+    k_sleep(K_SECONDS(1));
+  }
+  return 0;
 }
