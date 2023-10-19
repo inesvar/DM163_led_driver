4abcee6d388284e86f91b62e790985258fff3c00
inesvar <90340779+inesvar@users.noreply.github.com>
2023-10-19 17:04:39 +0200

    Locate and use the VL53L0X sensor : final version

--- src/main.c
+++ src/main.c
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2016 Open-RnD Sp. z o.o.
- * Copyright (c) 2020 Nordic Semiconductor ASA
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
@@ -22,38 +15,27 @@
  */
 static const struct device *const tof_sensor = DEVICE_DT_GET_ANY(st_vl53l0x);
 
-/*
- * The led0 devicetree alias is optional. If present, we'll use it
- * to turn on the LED whenever the button is pressed.
- */
-static struct gpio_dt_spec led = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios,
-                                                     {0});
+static int setup_tof_sensor();
 
 int main(void)
 {
-  // Checking for led errors
-  if (led.port && !gpio_is_ready_dt(&led))
+  if (!setup_tof_sensor())
+    return 1;
+
+  // Printing the distance measures from the tof sensor
+  while (1)
   {
-    printk("Error : LED device %s is not ready; ignoring it\n",
-           led.port->name);
-    return 0;
-  }
-  else if (led.port)
-  {
-    int ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT);
-    if (ret != 0)
-    {
-      printk("Error %d: failed to configure LED device %s pin %d\n",
-             ret, led.port->name, led.pin);
-      return 0;
-    }
-    else
-    {
-      printk("Set up LED at %s pin %d\n", led.port->name, led.pin);
-    }
+    struct sensor_value distance;
+    sensor_sample_fetch(tof_sensor);
+    sensor_channel_get(tof_sensor, SENSOR_CHAN_DISTANCE, &distance);
+    printk("distance: %d.%06d\n", distance.val1, distance.val2);
+    k_sleep(SLEEP_TIME_BETWEEN_MEASURES_MS);
   }
 
-  // Checking for sensor erros
+  return 0;
+}
+
+static int setup_tof_sensor() {
   if (tof_sensor == NULL)
   {
     printk("TOF sensor wasn't found.\n");
@@ -67,17 +49,6 @@ int main(void)
   else
   {
     printk("TOF sensor found and ready.\n");
+    return 1;
   }
-
-  // Printing the distance measures from the tof sensor
-  while (1)
-  {
-    struct sensor_value distance;
-    sensor_sample_fetch(tof_sensor);
-    sensor_channel_get(tof_sensor, SENSOR_CHAN_DISTANCE, &distance);
-    printk("distance: %d.%06d\n", distance.val1, distance.val2);
-    k_sleep(SLEEP_TIME_BETWEEN_MEASURES_MS);
-  }
-
-  return 0;
 }
