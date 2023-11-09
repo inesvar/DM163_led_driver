60bdbf20e132588c517acce005ed4eafd0428d82
inesvar <90340779+inesvar@users.noreply.github.com>
2023-11-09 18:52:02 +0100

    Led driver : driving the lines

--- dm163_example/src/main.c
+++ dm163_example/src/main.c
@@ -1,22 +1,44 @@
 #include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/led.h>
 #include <zephyr/kernel.h>
 
 #define DM163_NODE DT_NODELABEL(dm163)
 static const struct device *dm163_dev = DEVICE_DT_GET(DM163_NODE);
 
+#define RGB_MATRIX_NODE DT_NODELABEL(rgb_matrix)
+
+BUILD_ASSERT(DT_PROP_LEN(RGB_MATRIX_NODE, rows_gpios) == 8);
+
+static const struct gpio_dt_spec rows[] = {
+    GPIO_DT_SPEC_GET_BY_IDX(RGB_MATRIX_NODE, rows_gpios, 0),
+    GPIO_DT_SPEC_GET_BY_IDX(RGB_MATRIX_NODE, rows_gpios, 1),
+    GPIO_DT_SPEC_GET_BY_IDX(RGB_MATRIX_NODE, rows_gpios, 2),
+    GPIO_DT_SPEC_GET_BY_IDX(RGB_MATRIX_NODE, rows_gpios, 3),
+    GPIO_DT_SPEC_GET_BY_IDX(RGB_MATRIX_NODE, rows_gpios, 4),
+    GPIO_DT_SPEC_GET_BY_IDX(RGB_MATRIX_NODE, rows_gpios, 5),
+    GPIO_DT_SPEC_GET_BY_IDX(RGB_MATRIX_NODE, rows_gpios, 6),
+    GPIO_DT_SPEC_GET_BY_IDX(RGB_MATRIX_NODE, rows_gpios, 7),
+};
+
 int main() {
   if (!device_is_ready(dm163_dev)) {
     return -ENODEV;
   }
+  for (int row = 0; row < 8; row++)
+    gpio_pin_configure_dt(&rows[row], GPIO_OUTPUT_INACTIVE);
   // Set brightness to 5% for all leds so that we don't become blind
   for (int i = 0; i < 8; i++) led_set_brightness(dm163_dev, i, 5);
-  // Animate the leds
+  // Animate the leds on every row and every column
   for (;;) {
-    for (int col = 0; col < 8; col++) {
-      led_on(dm163_dev, col);
-      k_sleep(K_MSEC(100));
-      led_off(dm163_dev, col);
+    for (int row = 0; row < 8; row++) {
+      gpio_pin_set_dt(&rows[row], 1);
+      for (int col = 0; col < 8; col++) {
+        led_on(dm163_dev, col);
+        k_sleep(K_MSEC(30));
+        led_off(dm163_dev, col);
+      }
+      gpio_pin_set_dt(&rows[row], 0);
     }
   }
 }
